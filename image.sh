62085df1988c60a979349f2fa3c9242cd016d7f6
tregubov84 <tregubov84@gmail.com>
2021-05-26 17:25:20 +0700

    v1

--- image.sh
+++ image.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
+FILE=$1
 
-find . -name "*.jpg"
+function cropImage()
+{
+        NAME=$(echo $2 | cut -d'.' -f1)
+        EXTENSION=$(echo $2 | cut -d'.' -f2)
+        convert $2 -resize $1 $NAME"_thumbnail."$EXTENSION
+}
 
-identify -format '%w %h' "*.jpg"
-
-a=%w
-b=%h
-
-if $a<$b
-then convert *.jpg -resize x360 $NAME"_thumbnail".jpg  
-else convert *.jpg -resize  360 $NAME"_thumbnail".jpg
+if test -f "$FILE"; then
+        DIR=$(ls $FILE | awk '{print $1}' | grep '\.jpg$' | awk '!/_thumbnail/')
+else
+        DIR=$(du -a . | awk '{print $2}' | grep '\.jpg$' | awk '!/_thumbnail/')
 fi
+for file in $DIR; do
+        TEMPNAME=$(basename "$file")
+        RESOLUTION=$(convert {$TEMPNAME} -print "%w %h\n" /dev/null)
+        W=$(echo $RESOLUTION | awk '{print $1}')
+        H=$(echo $RESOLUTION | awk '{print $2}')
 
-
-
-
-
-
-
-
-
-
-
+        if [ $W -gt $H ]; then
+                cropImage "360x" $TEMPNAME
+        else
+                cropImage "x360" $TEMPNAME
+        fi
+done
 
